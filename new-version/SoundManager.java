9555e17d48ee67d7e74bd31c21048d6d3337dfb3
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-28 15:43:41 -0500

    new version game file

---
+++ new-version/SoundManager.java
@@ -0,0 +1,24 @@
+import java.io.InputStream;
+import javax.sound.sampled.*;
+
+public class SoundManager {
+    private static final String EAT_SOUND = "/asset/food.wav";
+    private static final String GAME_OVER_SOUND = "/asset/gameover.wav";
+
+    public void playSound(String soundFilePath) {
+        try {
+            // Load the sound file as a resource
+            InputStream audioSrc = getClass().getResourceAsStream(soundFilePath);
+            if (audioSrc == null) {
+                System.err.println("Sound file not found: " + soundFilePath);
+                return;
+            }
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioSrc);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
