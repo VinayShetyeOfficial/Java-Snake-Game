2347ba3c93bc5fe06d4bc2cc15723fb4555d3763
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-28 15:43:50 -0500

    new version game file

---
+++ old-version/SnakeGame.java
@@ -0,0 +1,11 @@
+public class SnakeGame {
+
+    // Entry point of the program
+    public static void main(String[] args) {
+        
+        // Create a new instance of the GameFrame class
+        // which represents the main window of the game
+        new GameFrame();
+    }
+
+}
